81ca4373ff3dec1edd94d75ab9b61d56b6699a10
blueback <blueback28@gmail.com>
2022-02-21 20:16:28 +0900

    整理

--- BlueBackAudio/Assets/UPM/Runtime/BlueBack/Audio/AudioMixer_MonoBehaviour.cs
+++ BlueBackAudio/Assets/UPM/Runtime/BlueBack/Audio/AudioMixer_MonoBehaviour.cs
@@ -1,9 +1,9 @@
 
 
 /**
- * Copyright (c) blueback
- * Released under the MIT License
- * @brief オーディオ。オーディオミキサー。
+	Copyright (c) blueback
+	Released under the MIT License
+	@brief オーディオ。オーディオミキサー。
 */
 
 
--- BlueBackAudio/Assets/UPM/Runtime/BlueBack/Audio/AudioSourceItem.cs
+++ BlueBackAudio/Assets/UPM/Runtime/BlueBack/Audio/AudioSourceItem.cs
@@ -1,9 +1,9 @@
 
 
 /**
- * Copyright (c) blueback
- * Released under the MIT License
- * @brief オーディオ。
+	Copyright (c) blueback
+	Released under the MIT License
+	@brief オーディオ。
 */
 
 
