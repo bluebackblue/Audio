e39804a9f7d3e3a31e1f730647b6d8f543da6d6e
blueback <blueback28@gmail.com>
2022-08-19 21:36:08 +0900

    整理

--- BlueBackAudio/Assets/UPM/Runtime/BlueBack/Audio/Version.cs
+++ BlueBackAudio/Assets/UPM/Runtime/BlueBack/Audio/Version.cs
@@ -21,7 +21,7 @@ namespace BlueBack.Audio
 
 		/** packageversion
 		*/
-		public const string packageversion = "0.0.21";
+		public const string packageversion = "0.0.22";
 
 		/** GetPackageVersion
 		*/
